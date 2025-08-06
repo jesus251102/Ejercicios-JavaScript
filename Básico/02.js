f11141fdef9ad2960393d06de0f833ff01f52ce6
Jesus Martínez <{email}>
2025-08-06 16:05:50 -0500

    Segundo commit

--- Básico/02.js
+++ Básico/02.js
@@ -1,6 +1,7 @@
 // Ingresa un número y muestra si es par o impar.
 
-let numero = prompt("Ingresa un número:");
+// let numero = prompt("Ingresa un número:");
+let numero = 5;
 if (numero % 2 === 0) {
     console.log(`El número ${numero} es par.`);
 } else {
---
+++ Básico/03.js
@@ -0,0 +1,11 @@
+let number1 = 5;
+let number2 = 10;
+let number3 = 7;
+
+if (number1 > number2 && number1 > number3) {
+    console.log(`El número mayor es ${number1}`);
+} else if (number2 > number1 && number2 > number3) {
+    console.log(`El número mayor es ${number2}`);
+} else {
+    console.log(`El número mayor es ${number3}`);
+}
