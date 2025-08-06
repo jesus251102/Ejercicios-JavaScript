3e98df3b54d3a7bbdc833a956d3f9989330d2eb6
Jesus Martínez <{email}>
2025-08-06 15:50:09 -0500

    Primer commit

---
+++ Básico/01.js
@@ -0,0 +1,6 @@
+// Crea una función que reciba dos números y retorne su suma.
+function sumar(a, b) {
+    return a + b;
+}
+
+console.log(sumar(3, 5));
---
+++ Básico/02.js
@@ -0,0 +1,8 @@
+// Ingresa un número y muestra si es par o impar.
+
+let numero = prompt("Ingresa un número:");
+if (numero % 2 === 0) {
+    console.log(`El número ${numero} es par.`);
+} else {
+    console.log(`El número ${numero} es impar.`);
+}
